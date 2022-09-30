aa09e9f8281bcc23731863e92f5ff6aca3df78d6
Víctor Gómez <vgomez@applaudostudios.com>
2022-09-30 11:26:29 -0400

    Finish unformal test at AdministrationPlayground

--- src/main/scala/com/vgomez/app/actors/AdministrationPlayground.scala
+++ src/main/scala/com/vgomez/app/actors/AdministrationPlayground.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.pattern.{ask, pipe}
 import akka.util.Timeout
 import com.vgomez.app.actors.Restaurant.RestaurantInfo
+import com.vgomez.app.actors.Review.ReviewInfo
 import com.vgomez.app.actors.User.UserInfo
 import com.vgomez.app.domain.DomainModel._
 import com.vgomez.app.domain.DomainModelFactory.generateNewEmptySchedule
@@ -33,6 +34,7 @@ object AdministrationPlayground extends App {
 
   // ids
   var restaurantId = "83ff1fac-3603-4954-b4bd-1eadd78de41e"
+  var reviewId = "d99803f4-4814-4fa7-95f5-d5c863995eb6"
 
 
   class SimpleActor extends Actor with ActorLogging {
@@ -71,6 +73,21 @@ object AdministrationPlayground extends App {
 //  (administration ? CreateRestaurant(RestaurantInfo("admin", "Sushi", "RD", "Santiago", "809",
 //    Location(0,0), Set("Sushi"), generateNewEmptySchedule()))).pipeTo(simpleActor)
 //
-  (administration ? GetRestaurant(restaurantId)).pipeTo(simpleActor)
 
+//   (administration ? UpdateRestaurant(restaurantId,RestaurantInfo("admin", "Sushi Ban", "RD mi pais", "Santiago0", "849",
+//      Location(0,1), Set("Sushi", "Ensalada"), generateNewEmptySchedule()))).pipeTo(simpleActor)
+//
+//  (administration ? DeleteRestaurant(restaurantId)).pipeTo(simpleActor)
+//
+//  (administration ? GetRestaurant(restaurantId)).pipeTo(simpleActor)
+
+
+  // Review
+  // (administration ? CreateReview(ReviewInfo("admin",restaurantId, 5, "", "09-31-2022"))).pipeTo(simpleActor)
+
+  (administration ? UpdateReview(reviewId ,ReviewInfo("admin",restaurantId, 4, "", "09-30-2022"))).pipeTo(simpleActor)
+
+  (administration ? DeleteReview(reviewId)).pipeTo(simpleActor)
+
+  (administration ? GetReview(reviewId)).pipeTo(simpleActor)
 }
