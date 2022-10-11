b2030bba47ae7edebae8c5923947c9a4b223a42a
Víctor Gómez <vgomez@applaudostudios.com>
2022-10-11 12:03:05 -0400

    Use Option on convertRowToMapCommands/LoadDataset

--- src/main/scala/com/vgomez/app/loadDataset/LoadDataset.scala
+++ src/main/scala/com/vgomez/app/loadDataset/LoadDataset.scala
@@ -18,19 +18,66 @@ import java.util.UUID
 
 import scala.concurrent.ExecutionContext
 
+object LoadDataset{
+  def convertRowToMapCommands(row: Map[String, String]): Map[String, Product] = {
+    val locationField: Location = Location(row.getOrElse("latitude", "0").toDouble,
+      row.getOrElse("longitude", "0").toDouble)
+
+    val categoriesField: Set[String] = row.getOrElse("categories", "").split(",").map(category => category.trim).toSet
+    val restaurantId: String = row.getOrElse("business_id", UUID.randomUUID().toString)
+    val reviewId: String = row.getOrElse("review_id", UUID.randomUUID().toString)
+    val username: String = row.getOrElse("user_id", UUID.randomUUID().toString)
+
+
+    val createUserCommand = getCreateUserCommand(row, locationField, categoriesField)
+    val createRestaurantCommand = getCreateRestaurantCommand(row, locationField, categoriesField, restaurantId, username)
+    val createReviewCommand = getCreateReviewCommand(row, restaurantId, reviewId, username)
+
+    Map("createUserCommand" -> createUserCommand, "createRestaurantCommand" -> createRestaurantCommand,
+      "createReviewCommand" -> createReviewCommand)
+  }
+
+  def getCreateUserCommand(row: Map[String, String], locationField: Location,
+                           categoriesField: Set[String]): CreateUser = {
+    CreateUser(UserInfo(username = row.getOrElse("user_id", UUID.randomUUID().toString),
+      password = UUID.randomUUID().toString,
+      role = Normal,
+      location = locationField,
+      favoriteCategories = categoriesField))
+  }
+
+  def getCreateRestaurantCommand(row: Map[String, String], locationField: Location,
+                                 categoriesField: Set[String], restaurantId: String, username: String): CreateRestaurant = {
+    val defaultHours: String = "{'Monday': '0:0-0:0'}"
+
+    CreateRestaurant(maybeId = Some(restaurantId), RestaurantInfo(
+      userId = username, name = row.getOrElse("name", "Unknown name"), state = row.getOrElse("state_", "Unknown state"),
+      city = row.getOrElse("city", "Unknown city"), postalCode = row.getOrElse("postal_code", "UnKnown postal code"),
+      location = locationField, categories = categoriesField,
+      schedule = transformScheduleStringToSchedule(row.getOrElse("hours", defaultHours))
+    ))
+  }
+
+  def getCreateReviewCommand(row: Map[String, String], restaurantId: String, reviewId: String,
+                             username: String): CreateReview = {
+    CreateReview(maybeId = Some(reviewId),
+      ReviewInfo(userId = username, restaurantId = restaurantId,
+        stars = row.getOrElse("customer_stars", "0").toInt, text = row.getOrElse("text_", "No text"),
+        date = row.getOrElse("date_", "Unknown date")))
+  }
+}
+
 class LoadDataset(filePath: String, administration: ActorRef, implicit val system: ActorSystem,
                   implicit val timeout: Timeout) {
+    import LoadDataset._
     // Use Akka Stream to process the data
     implicit val materializer = ActorMaterializer()
     implicit val scheduler: ExecutionContext = system.dispatcher
 
 
   def runLoadDataSetGraph() = {
-    /*
-    Todo ask about why it only process a maximum of 15 row.
-    */
     val readerStream = getReaderStream().take(15)
-    println(s"readerStream size: ${readerStream.size}")
+
     // Graph
     val source = Source(readerStream)
     val flow = Flow[Map[String, String]].map(row => convertRowToMapCommands(row))
@@ -39,54 +86,13 @@ class LoadDataset(filePath: String, administration: ActorRef, implicit val syste
            administration ! commandsMap("createRestaurantCommand")
            administration ! commandsMap("createReviewCommand")
         })
-//    var i = 0
-//    val sink = Sink.foreach{_: Map[String, Product] => {
-//      i = i + 1
-//      println(i)
-//    }}
+
     source.via(flow).to(sink).run()
   }
 
   def getReaderStream(): Stream[Map[String, String]] = {
     val reader = CSVReader.open(new File(filePath))
     reader.toStreamWithHeaders
-  }
-
-
-  def convertRowToMapCommands(row: Map[String, String]): Map[String, Product]  = {
-    val locationField: Location = Location(row("latitude").toDouble, row("longitude").toDouble)
-    val categoriesField: Set[String] = row("categories").split(",").map(category => category.trim).toSet
-
-    val createUserCommand = getCreateUserCommand(row, locationField, categoriesField)
-    val createRestaurantCommand = getCreateRestaurantCommand(row, locationField, categoriesField)
-    val createReviewCommand = getCreateReviewCommand(row)
-
-    Map("createUserCommand" -> createUserCommand, "createRestaurantCommand" -> createRestaurantCommand,
-      "createReviewCommand" -> createReviewCommand)
-  }
-
-  def getCreateUserCommand(row: Map[String, String], locationField: Location,
-                           categoriesField: Set[String]): CreateUser = {
-    CreateUser(UserInfo(username = row("user_id"),
-      password = UUID.randomUUID().toString,
-      role = Normal,
-      location = locationField,
-      favoriteCategories = categoriesField))
-  }
-
-  def getCreateRestaurantCommand(row: Map[String, String], locationField: Location,
-                                 categoriesField: Set[String]): CreateRestaurant = {
-    CreateRestaurant(maybeId = Some(row("business_id")), RestaurantInfo(
-      userId = row("user_id"), name = row("name"), state = row("state_"), city = row("postal_code"),
-      postalCode = row("postal_code"), location = locationField, categories = categoriesField,
-      schedule = transformScheduleStringToSchedule(row("hours"))
-    ))
-  }
-
-  def getCreateReviewCommand(row: Map[String, String]): CreateReview = {
-    CreateReview(maybeId = Some(row("review_id")),
-      ReviewInfo(userId = row("user_id"), restaurantId = row("business_id"),
-        stars = row("customer_stars").toInt, text = row("text_"), date = row("date_")))
   }
 
 }
